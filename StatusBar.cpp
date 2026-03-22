6158c073ccc5297e454d5ce7abeb66a735175852
VikaCarv <vikacarv0@gmail.com>
2026-03-22 08:59:28 -0400

    feat(darkmode): fase 10 - barra de status escura com texto rosa cyberpunk

--- StatusBar.cpp
+++ StatusBar.cpp
@@ -16,11 +16,11 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include "StdAfx.h"   
-#include "StatusBar.h"   
+#include "StdAfx.h"
+#include "StatusBar.h"
 #include "global.h"
 
- // StatusBar   
+ // StatusBar
 IMPLEMENT_DYNAMIC(StatusBar, CStatusBar)
 StatusBar::StatusBar()
 {
@@ -32,11 +32,13 @@ StatusBar::~StatusBar()
 }
 
 BEGIN_MESSAGE_MAP(StatusBar, CStatusBar)
-	//{{AFX_MSG_MAP(StatusBar)   
+	//{{AFX_MSG_MAP(StatusBar)
 	ON_MESSAGE(WM_IDLEUPDATECMDUI, OnIdleUpdateCmdUI)
 	ON_WM_LBUTTONUP()
 	ON_WM_MOUSEMOVE()
-	//}}AFX_MSG_MAP   
+	//}}AFX_MSG_MAP
+	ON_WM_ERASEBKGND()
+	ON_WM_PAINT()
 END_MESSAGE_MAP()
 
 LRESULT StatusBar::OnIdleUpdateCmdUI(WPARAM wParam, LPARAM lParam)
@@ -57,3 +59,44 @@ void StatusBar::OnLButtonUp(UINT nFlags, CPoint point)
 void StatusBar::OnMouseMove(UINT nFlags, CPoint point)
 {
 }
+// ===== DARK MODE =====
+BOOL StatusBar::OnEraseBkgnd(CDC* pDC)
+{
+    CRect rect;
+    GetClientRect(&rect);
+    pDC->FillSolidRect(&rect, RGB(20, 20, 20));
+    return TRUE;
+}
+
+void StatusBar::OnPaint()
+{
+    CPaintDC dc(this);
+    CRect rect;
+    GetClientRect(&rect);
+    dc.FillSolidRect(&rect, RGB(20, 20, 20));
+    dc.SetTextColor(RGB(180, 20, 90));
+    dc.SetBkMode(TRANSPARENT);
+
+    int nCount = GetCount();
+    for (int i = 0; i < nCount; i++) {
+        CRect paneRect;
+        GetItemRect(i, &paneRect);
+        CString text;
+        UINT nID, nStyle;
+        int nWidth;
+        GetPaneInfo(i, nID, nStyle, nWidth);
+        GetPaneText(i, text);
+        if (i == 0) {
+            // ícone
+            HICON hIcon = GetStatusBarCtrl().GetIcon(i);
+            if (hIcon) {
+                ::DrawIconEx(dc.m_hDC, paneRect.left + 2,
+                    paneRect.top + (paneRect.Height() - 16) / 2,
+                    hIcon, 16, 16, 0, NULL, DI_NORMAL);
+                paneRect.left += 20;
+            }
+        }
+        dc.DrawText(text, &paneRect, DT_LEFT | DT_VCENTER | DT_SINGLELINE);
+    }
+}
+// ===== FIM DARK MODE =====
--- StatusBar.h
+++ StatusBar.h
@@ -1,17 +1,21 @@
-#pragma once 
+#pragma once
 
 #include "const.h"
 
 class StatusBar : public CStatusBar
-{ 
-	DECLARE_DYNAMIC(StatusBar) 
-public: 
+{
+	DECLARE_DYNAMIC(StatusBar)
+public:
 	StatusBar();
 	virtual ~StatusBar();
-protected: 
+protected:
 	DECLARE_MESSAGE_MAP()
-public: 
+public:
 	afx_msg LRESULT OnIdleUpdateCmdUI(WPARAM wParam,LPARAM lParam);
 	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
 	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	// ===== DARK MODE =====
+    afx_msg BOOL OnEraseBkgnd(CDC* pDC);
+    afx_msg void OnPaint();
+    // ===== FIM DARK MODE =====
 };
